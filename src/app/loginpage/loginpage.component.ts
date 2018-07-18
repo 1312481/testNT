2531bae352e2b0eacd9b5ed75f39f5a6dca8c544
HARVEYNASH\sangnguyenq <nguyenquangsang2810@gmail.com>
2018-07-18 11:08:44 +0700

    login page

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class LoginpageComponent implements OnInit {
 
   constructor() { }
-
+  w = screen.width;
   ngOnInit() {
+    console.log(this.w);
   }
 
 }
